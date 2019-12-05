e150ade16cdc3fc44cc6d93f27e37757a1bb2f69
Anthony G <adgiallom@gmail.com>
2019-12-05 13:04:32 -0500

    Wrapper for pingbcast

--- bin/pingbcast.sh
+++ bin/pingbcast.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-V="$(uname -a | awk '{print $3})'"
-
-echo "${V}"
-
-BCAST="$(ip a | grep brd | grep inet | awk '{print $4}')"
-
-echo "${BCAST}"
+#BCAST="$(ip a | grep brd | grep inet | awk '{print $4}')"
+#ping "${BCAST}"
+ping -b `ip a | grep brd | grep inet | awk '{print $4}'`
