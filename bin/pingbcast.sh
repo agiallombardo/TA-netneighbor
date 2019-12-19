ba0b064a025e239b210a13085b1a106bc5d6c167
Anthony G <adgiallom@gmail.com>
2019-12-19 09:16:12 -0500

    Create Inputs, finalize output and props

--- bin/pingbcast.sh
+++ bin/pingbcast.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-for i in $(ip -4 a | grep brd | awk '{print $4}'); do timeout 5s ping -b $i; done
+export DATE=`date "+%m-%d-%Y %H:%M:%S.%3N %z"`
+export HOSTNAME=`hostname`
+for i in $(ip -4 a | grep brd | awk '{print $4}'); do timeout 5s ping -b $i; done; arp -ev
